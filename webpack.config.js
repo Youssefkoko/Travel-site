75f844b620d139d6fcf2a7a84016a3d137e36b9f
Sonja <youssefkoko01@gmail.com>
2020-12-17 17:28:50 +0100

    Adding  webpack serve for loading browser seamlessly and inje

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,16 @@ module.exports = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    before: function(app, server){
+      server._watch('./app/**/*.html')
+    },
+    host: 'x.x.x.x.xIP',
+  },
   mode: 'development',
-  watch: true,
   module: {
     rules: [
       {
